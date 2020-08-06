e0faa8d3a95a55d6126dad68eb33d5b0303ab49c
alecristia <alecristia@gmail.com>
2020-08-06 15:20:47 +0200

    drawing data from google drive

--- LangComplex.R
+++ LangComplex.R
@@ -2,19 +2,28 @@ library(tidyverse)
 library(ggplot2)
 library(gvlma) 
 library(car)
+library(RCurl)
 
-all_data <- read_delim("./Data/CR_alldata.csv", delim=';',col_types = cols(.default = "c"))
+docloc='https://docs.google.com/spreadsheets/d/e/2PACX-1vSzvJcT6yT9_fpRoFg5O7LAput7VKKltSxAuGMyC5wDlo_75D9ELA8YaVeMIVwcLw/pub?gid=1294110857&single=true&output=csv'
+myfile <- getURL(docloc, ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
+all_data<- read.csv(textConnection(myfile), header=T)
 
+summary(all_data)
+dim(all_data)
+
+
+# apply exclusions
+#data.sub <- subset(all_data,  Age_in_months<=50)
+#removed: corpus != "Warlaumont" & corpus != "Cychosz" & because we decided to include bilinguals
+data.sub<-all_data
 
 #correct some data issues
-data.sub$CR=as.numeric(data.sub$CR)
-data.sub$SylComp=factor(data.sub$`Syllable complexity`,levels=c("Low","Moderate","High"))
-data.sub$Age=as.numeric(data.sub$`Age in months`)
+data.sub$CR=as.numeric(gsub(",",".",data.sub$CR))
+data.sub$SylComp=factor(data.sub$Syllable.complexity,levels=c("Low","Moderate","High"))
+data.sub$Age=as.numeric(gsub(",",".",data.sub$Age.in.months))
 data.sub$Age2=data.sub$Age^2 #generate squared component
 data.sub$Age3=data.sub$Age^3 #generate cubic component
 
-# apply exclusions
-data.sub <- subset(all_data, corpus != "Warlaumont" & corpus != "Cychosz" & `Age in months`<=50)
 
 # add more information
 data.sub$coding<-ifelse(data.sub$corpus %in% c("Solomon","French"),"lab","citsci")
@@ -54,30 +63,41 @@ anova(mod_complex,mod_age) #no, model with interaction terms is much better than
 # check whether it's the interaction
 mod_int=lm(CR~Age*SylComp,data=data.sub)
 anova(mod_complex,mod_int) 
-#indirectly, it seems so, because the more complex model is only marginally better than this simpler one, with interaction
+#NOTE! This changed with more data
+# I first thought it seems so, because the more complex model is only marginally better than this simpler one, with interaction
+#but now the more complex model is sig better than this simpler one
 
 # add back polynomials but without interaction
 mod_int_age=lm(CR~Age*SylComp+Age2+Age3,data=data.sub)
-anova(mod_complex,mod_int_age) #model with interactions on all the polyn terms is no better
+anova(mod_complex,mod_int_age) 
+#NOTE: this also changed with more data
+# originally I thought model with interactions on all the polyn terms is no better
+# but now it is
+
 mod_int_age2=lm(CR~Age*SylComp+Age2,data=data.sub)
 anova(mod_int_age,mod_int_age2) #and age cube doesn't add anything either
+#this hasn't changed-- age cube didn't help
+
+mod_int_age2_int=lm(CR~Age*SylComp+Age2*SylComp,data=data.sub)
+anova(mod_complex,mod_int_age2_int) #ah note that the interaction age3*sylcomp wasn't adding anything
+
 
 #check for assumptions in this new winning model
-plot(mod_int_age) #looks ok
-gvlma(mod_int_age) #passes all checks
+plot(mod_int_age2_int) #looks ok
+gvlma(mod_int_age2_int) #passes all checks
 
 # So look at what it says
-Anova(mod_int_age, type="III") #main effect of age, syllable complexity, & marginal interaction
-summary(mod_int_age) #this suggests that to a certain extent, these results are driven by the Tsimane
-# so let's check what happens without Tsi
+Anova(mod_int_age2_int, type="III") 
+summary(mod_int_age2_int) 
+#main effect of age, ag2, sylcomp, interaction age*sylcom and age2*sylcom!
 
-mod_int_age_noTsi=lm(CR~Age*SylComp+Age2+Age3,data=data.sub,subset=c(corpus!="Tsimane"))
+mod_int_age_noTsi=lm(CR~Age*SylComp+Age2*SylComp,data=data.sub,subset=c(corpus!="Tsimane"))
 plot(mod_int_age_noTsi) #looks ok
 gvlma(mod_int_age_noTsi) #passes all checks
 
 Anova(mod_int_age_noTsi, type="III")
 summary(mod_int_age_noTsi)
-#yes, driven by Tsimane 
+#results are not driven by Tsimane 
 
 # plot data
 ggplot(data.sub, aes(x=Age, y=CR, color=SylComp)) +
@@ -86,24 +106,23 @@ ggplot(data.sub, aes(x=Age, y=CR, color=SylComp)) +
   geom_smooth(method=lm,se=FALSE)
 
 
-# force starting point at zero
-mod_int_age_noTsi_intzero=lm(CR~0+Age*SylComp+Age2+Age3,data=data.sub,subset=c(corpus!="Tsimane"))
-plot(mod_int_age_noTsi_intzero) #looks ok
-gvlma(mod_int_age_noTsi_intzero) #checks cannot be done (perhaps you cannot have intercept = zero)
-Anova(mod_int_age_noTsi_intzero, type="III") 
-summary(mod_int_age_noTsi_intzero)
-# only main effect of age
-
 # scale ages, so that intercept corresponds to mean age
 data.sub$Age.s=scale(data.sub$Age)
 data.sub$Age2.s=scale(data.sub$Age2) 
 data.sub$Age3.s=scale(data.sub$Age3) 
 
 # better control for ages...
-mod_int_age_noTsi_ageScaled=lm(CR~Age.s*SylComp+Age2.s+Age3.s,data=data.sub,subset=c(corpus!="Tsimane"))
+mod_int_age_noTsi_ageScaled=lm(CR~Age.s*SylComp+Age2.s*SylComp,data=data.sub,subset=c(corpus!="Tsimane"))
 plot(mod_int_age_noTsi_ageScaled) #looks ok
 gvlma(mod_int_age_noTsi_ageScaled) #checks ok
 Anova(mod_int_age_noTsi_ageScaled, type="III") 
 summary(mod_int_age_noTsi_ageScaled)
 # now we have effect of syllable complexity
 
+# check that this is not just driven by Yeli old kids
+mod_int_age_noTsi_ageScaled_no_old=lm(CR~Age.s*SylComp+Age2.s*SylComp,data=data.sub,
+                                      subset=c(corpus!="Tsimane"&Age<40))
+plot(mod_int_age_noTsi_ageScaled_no_old) #looks ok
+gvlma(mod_int_age_noTsi_ageScaled_no_old) #checks ok
+Anova(mod_int_age_noTsi_ageScaled_no_old, type="III") 
+summary(mod_int_age_noTsi_ageScaled_no_old)
