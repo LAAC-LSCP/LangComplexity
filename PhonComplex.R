0ba4b5236e7b14b86b30cb43c6d4cc727c6f90fb
Natalia Kuzminykh <natkuzminykh94@gmail.com>
2021-02-10 16:30:36 +0100

    Updated some missing phon. data
    Now we have 111/120 young children to analyse

--- PhonComplex.R
+++ PhonComplex.R
@@ -29,14 +29,14 @@ mydata$corp <- as.factor(mydata$corpus)
 mydata$syl_comp <- as.factor(mydata$`Syllable complexity`)
 mydata$C_count <- as.factor(mydata$C_count)
 mydata$V_count <- as.factor(mydata$V_count)
-#mydata$mult_lik <- as.factor(mydata$Multinlingualism_likelihood)
+mydata$mult_lik <- as.factor(mydata$Multinlingualism_likelihood)
 
 
 #lets drop anything irrelevant
 mydata$ChildID <- NULL
 mydata$`ITS` <- NULL
 mydata$Multinlingualism_likelihood <- NULL
-mydata$mult_lik <- NULL
+#mydata$mult_lik <- NULL
 mydata$`Syllable complexity` <- NULL
 mydata$`Syllable.complexity` <- NULL
 mydata$`Age in months` <- NULL
@@ -50,7 +50,7 @@ mydata$corpus <- NULL
 
 #DATASET WITH PHONETIC PROPERTIES ---------------------------------
 #adding a file with phonetic data  
-phon_data<- read_excel("C:/Users/Lenovo/Desktop/UNISI/ENS Traineeship/GIT/LangComplexityN/Data/LAAC_Internship2020_Languages.xlsx")
+phon_data<- read_excel("C:/Users/Lenovo/Desktop/UNISI/ENS Traineeship/GIT/LangComplexity/Data/LAAC_Internship2020_Languages_upd.xlsx")
 
 #MERGING TWO DATASETS TOGETHER
 #select the columns to merge from the Languages file
@@ -59,7 +59,8 @@ mydata_sub$lang <- as.factor(mydata_sub$Language)
 mydata_sub$Language <- NULL
 
 #merge the selected columns into one dataset
-mydata_sub<-merge(mydata,mydata_sub, by="lang") #it leaves only 83 children, apparently because of the absence metada for som languages
+mydata_sub<-merge(mydata,mydata_sub, by="lang", all=T) #it leaves only 83 children, apparently because of the absence metada for som languages THIS WAY DUPLICATE SOME DATA 
+mydata_sub <- dplyr::distinct(mydata_sub) # TO GET RID OF DUPLICATES
 
 
 mydata_sub$Mad_syl_comp <- as.factor(mydata_sub$`Maddieson_sylcomp`)
@@ -71,6 +72,9 @@ mydata_sub$Maddieson_C_inv <- NULL
 mydata_sub$Maddieson_VQ_Inv <- NULL
 
 
+mydata_sub<-subset(mydata_sub, !is.na(CR)) #to get rid of empty CR entries
+
+
 #FINAL DATASET  ---------------------------------
 summary(mydata_sub)
 dim(mydata_sub)
@@ -80,8 +84,10 @@ dim(mydata_sub)
 mydata_sub$age2=mydata_sub$age^2 #generate squared component
 mydata_sub$age3=mydata_sub$age^3 #generate cubic component
 
-mydata_sub<-subset(mydata_sub, !is.na(Mad_C)) #NA are excluded -> 75 children
+mydata_sub<-subset(mydata_sub, !is.na(Mad_C)) #NA are excluded -> 111 children
 #mydata_sub$coding<-ifelse(mydata_sub$corp %in% c("Solomon","French"),"lab","citsci") # add more information
+
+
 
 # describe data
 table(mydata_sub$corp)
@@ -113,10 +119,12 @@ ggplot(mydata_sub,aes(x=CR,fill=Mad_VQ))+
 
 
 ggplot(mydata_sub, aes(x=age, y=CR, color=lang)) +
+  labs(colour = "Languages", shape="Languages") +
+  labs(x = "Age (months)")+
+  labs(y = "CP")+
   geom_point()+
   # Add regression lines
   geom_smooth(method=lm,se=FALSE)
-
 
 #MODELS  ---------------------------------
 # Fit most complex models
@@ -125,26 +133,26 @@ mod_complexV=lm(CR~age*Mad_VQ+age2*Mad_VQ+age3*Mad_VQ,data=mydata_sub) #Vowels
 
 #check for assumptions
 #Consonants
-plot(mod_complexC) #ERROR: some NaN appears
-gvlma(mod_complexC) #ERROR
+plot(mod_complexC) #not sure about fitted values and leverage
+gvlma(mod_complexC) #ERROR in solve.default(sigwhat)  
 #Vowels
-plot(mod_complexV) #ERROR: some NaN appears
+plot(mod_complexV) #not sure about fitted values and leverage
 gvlma(mod_complexV) #assumptions satisfied :)
 
 
 #compare to simpler model 
 #Consonants
 mod_simpleC=lm(CR~age+Mad_C,data=mydata_sub) 
-anova(mod_complexC,mod_simpleC) #doesn't seem to be significant :(, probably data are not enough
+anova(mod_complexC,mod_simpleC) # now there's a significance 0.05 '.'
 #Vowels
 mod_simpleV=lm(CR~age+Mad_VQ,data=mydata_sub) 
-anova(mod_complexV,mod_simpleV) # a slight significance -  0.01 '*' so far this is the best result
+anova(mod_complexV,mod_simpleV) # a slight significance -  0.01 '*'  
 
 
 # check whether it's simply due to age polynomials
 mod_age=lm(CR~age+age2+age3,data=mydata_sub)
-anova(mod_complexC,mod_age) #Consonants no significance
-anova(mod_complexV,mod_age) #Vowels # a slight significance -  0.001 '**' so far this is the best result
+anova(mod_complexC,mod_age) #now Consonants are significant 0.001 '**'
+anova(mod_complexV,mod_age) #Vowels are a slight significant -  0.001 '**' so far this is the best result
  
 
 # check whether it's the interaction
@@ -153,16 +161,16 @@ mod_intC=lm(CR~age*Mad_C,data=mydata_sub)
 anova(mod_complexC,mod_intC) #no significance
 #Vowels
 mod_intV=lm(CR~age*Mad_VQ,data=mydata_sub)
-anova(mod_complexV,mod_intV) #0.05 '.'
+anova(mod_complexV,mod_intV) #0.01 '*'
 
 
 # add back polynomials but without interaction
 #Consonants 
 mod_int_ageC=lm(CR~age*Mad_C+age2+age3,data=mydata_sub)
-anova(mod_complexC,mod_int_ageC) #no significance
+anova(mod_complexC,mod_int_ageC) #0.01 '*'
 #Vowels
 mod_int_ageV=lm(CR~age*Mad_VQ+age2+age3,data=mydata_sub)
-anova(mod_complexV,mod_int_ageV) # a slight significance -  0.01 '*' so far this is the best result
+anova(mod_complexV,mod_int_ageV) # a slight significance -  0.001 '**' so far this is the best result
  
 
 #Consonants 
@@ -174,19 +182,19 @@ anova(mod_int_ageV,mod_int_ageV2) #the results are less significant without age 
 
 #Consonants 
 mod_int_age2_intC=lm(CR~age*Mad_C+age2*Mad_C,data=mydata_sub)
-anova(mod_complexC,mod_int_age2_intC) #an interaction age3*sylcomp doesn't change anything
+anova(mod_complexC,mod_int_age2_intC) #an interaction age3*sylcomp doesn't give any significance anything
 #Vowels
 mod_int_age2_intV=lm(CR~age*Mad_VQ+age2*Mad_VQ,data=mydata_sub)
-anova(mod_complexV,mod_int_age2_intV) # the interaction gives the same significance 0.01 '*'
+anova(mod_complexV,mod_int_age2_intV) # the interaction gives the same significance 0.001 '**'
 
 
 #check for assumptions in the last model  
 #Consonants 
-plot(mod_int_age2_intC) 
-gvlma(mod_int_age2_intC)  #satisfied
+plot(mod_int_age2_intC)  #not sure about fitted values and leverage
+gvlma(mod_int_age2_intC)  #Global Stat and Heteroscedasticity  are not satisfied
 #Vowels
-plot(mod_int_age2_intV) 
-gvlma(mod_int_age2_intV)  #Link Function is not satisfied
+plot(mod_int_age2_intV)  #not sure about leverage
+gvlma(mod_int_age2_intV)  #Link Function and Global Stat are not satisfied
 plot(mod_int_ageV) 
 gvlma(mod_int_ageV) #NOTE it's satisfied in the model without interaction
 
@@ -194,7 +202,7 @@ gvlma(mod_int_ageV) #NOTE it's satisfied in the model without interaction
 
 # So look at what it says
 #Consonants 
-Anova(mod_int_age2_intC, type="III") #not very impressive
+Anova(mod_int_age2_intC, type="III") #it looks intersting
 summary(mod_int_age2_intC) 
 #Vowels
 Anova(mod_int_age2_intV, type="III") #still worse with an interaction
@@ -211,7 +219,7 @@ gvlma(mod_int_age_noTsiC)   #passes all checks
 #Vowels
 mod_int_age_noTsiV=lm(CR~age*Mad_VQ+age2*Mad_VQ,data=mydata_sub,subset=c(corp!="Tsimane"))
 plot(mod_int_age_noTsiV)  
-gvlma(mod_int_age_noTsiV) #Link Function is not satisfied
+gvlma(mod_int_age_noTsiV) #Link Function and Global Stat are not satisfied
 
 
 #Consonants 
@@ -257,7 +265,7 @@ summary(mod_int_age_noTsi_ageScaledC)
 #Vowels
 mod_int_age_noTsi_ageScaledV=lm(CR~age.s*Mad_VQ+age2.s*Mad_VQ,data=mydata_sub,subset=c(corp!="Tsimane"))
 plot(mod_int_age_noTsi_ageScaledV)  
-gvlma(mod_int_age_noTsi_ageScaledV)  #same problem with Link Function
+gvlma(mod_int_age_noTsi_ageScaledV)  #same problem with Link Function and Global Stat
 Anova(mod_int_age_noTsi_ageScaledV, type="III") 
 summary(mod_int_age_noTsi_ageScaledV)
 
@@ -275,7 +283,7 @@ summary(mod_int_age_noTsi_ageScaled_no_oldC)
 mod_int_age_noTsi_ageScaled_no_oldV=lm(CR~age.s*Mad_VQ+age2.s*Mad_VQ,data=mydata_sub,
                                        subset=c(corp!="Tsimane"&age<40))
 plot(mod_int_age_noTsi_ageScaled_no_oldV)  
-gvlma(mod_int_age_noTsi_ageScaled_no_oldV) #Link Funktion is not satisfied
+gvlma(mod_int_age_noTsi_ageScaled_no_oldV) #Link Funktion and Global stat are not satisfied
 Anova(mod_int_age_noTsi_ageScaled_no_oldV, type="III") 
 summary(mod_int_age_noTsi_ageScaled_no_oldV)
 
@@ -288,8 +296,8 @@ ggplot(mydata_sub, aes(x=age, y=CR, color=Mad_C, shape=Mad_C)) +
   theme_bw()+
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())+
-  scale_color_manual(values=c("#E69F00", "#56B4E9", "#009E73"))+
-  scale_shape_manual(values=c(4, 16, 0))+
+ # scale_color_manual(values=c("#E69F00", "#56B4E9", "#009E73"))+
+ # scale_shape_manual(values=c(4, 16, 0))+
   labs(x = "Age (months)")+
   labs(y = "CP")+
   geom_point(size=2.5)+
@@ -340,9 +348,9 @@ mydata_sub_S <- subset(mydata_sub,  Mad_C=="Moderately Small")
 mydata_sub_A <- subset(mydata_sub,  Mad_C=="Average")
 mydata_sub_L <- subset(mydata_sub,  Mad_C=="Moderately large")
 
-"Moderately Small Mad_C"; cor.test(mydata_sub_S$age,mydata_sub_S$CR) #0.9008206 
-"Average Mad_C"; cor.test(mydata_sub_A$age,mydata_sub_A$CR) #0.6029925
-"Moderately large Mad_C"; cor.test(mydata_sub_L$age,mydata_sub_L$CR) #0.2391876  Pretty low
+"Moderately Small Mad_C"; cor.test(mydata_sub_S$age,mydata_sub_S$CR) #0.8752329 
+"Average Mad_C"; cor.test(mydata_sub_A$age,mydata_sub_A$CR) #0.4389019 
+"Moderately large Mad_C"; cor.test(mydata_sub_L$age,mydata_sub_L$CR) #0.2391876   Pretty low
 
 
 #check assumptions
@@ -389,8 +397,8 @@ plot(lm_SvL, main="Small vs. Large")#looks quite bad
 mydata_sub_Av <- subset(mydata_sub,  Mad_VQ=="Average")
 mydata_sub_Lr <- subset(mydata_sub,  Mad_VQ=="Large")
 
-"Average Mad_VQ"; cor.test(mydata_sub_Av$age,mydata_sub_Av$CR)  #0.6447247
-"Large Mad_VQ"; cor.test(mydata_sub_Lr$age,mydata_sub_Lr$CR)  #0.7149718 
+"Average Mad_VQ"; cor.test(mydata_sub_Av$age,mydata_sub_Av$CR)  #0.6233493
+"Large Mad_VQ"; cor.test(mydata_sub_Lr$age,mydata_sub_Lr$CR)  #0.6149568
 
 
 #check assumptions
@@ -411,3 +419,9 @@ par(mfrow=c(2,2))
 plot(lm_AvvLr, main="Average vs. Large")
 "Average vs. Large"; anova(lm(AvvLr$CR~AvvLr$age*AvvLr$Mad_VQ))
 
+
+#___________
+attach(mydata_sub)
+lm_data <- lm(CR ~ age + gender + mult_lik + Mad_syl_comp + Mad_VQ + Mad_C)
+summary(lm_data)
+summary(lm(age ~ CR + gender + Mad_syl_comp + Mad_VQ + Mad_C))
